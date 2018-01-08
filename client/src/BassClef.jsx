99ac6c6cfcc8ad4a72d03927ff174dbe82116889
Ethan Lipkind <Ethan.lipkind@gmail.com>
2018-01-08 13:53:22 -0500

    clef positioning

--- client/src/BassClef.jsx
+++ client/src/BassClef.jsx
@@ -8,18 +8,23 @@ class BassClef extends React.Component {
 
 	render() {
 		return (
+			<div>
+				<div>
+					<img src="./images/bass.png" className="bassClef" alt=""/>
+				</div>
 				<div>
 					<div className="space" id="b3">B</div>
 					<div className="line" id="a3"><hr/></div>
-					<div className="space" id="g3">G</div>
+					<div className="space" id="g3"></div>
 					<div className="line" id="f3"><hr/></div>
-					<div className="space" id="e3">E</div>
+					<div className="space" id="e3"></div>
 					<div className="line" id="d3"><hr/></div>
-					<div className="space" id="c3">C</div>
+					<div className="space" id="c3"></div>
 					<div className="line" id="b2"><hr/></div>
-					<div className="space" id="a2">A</div>
+					<div className="space" id="a2"></div>
 					<div className="line" id="g2"><hr/></div>
 				</div>
+			</div>
 			);
 	}
 }
--- client/src/TrebleClef.jsx
+++ client/src/TrebleClef.jsx
@@ -8,19 +8,24 @@ class TrebleClef extends React.Component {
 
 	render() {
 		return (
+			<div>
+				<div>
+					<img src="./images/treble.png" className="trebleClef" alt=""/>
+				</div>
 				<div>
 					<div className="line" id="f6"><hr/></div>
-					<div className="space" id="e6">E</div>
+					<div className="space" id="e6"></div>
 					<div className="line" id="d6"><hr/></div>
-					<div className="space" id="c5">C</div>
+					<div className="space" id="c5"></div>
 					<div className="line" id="b4"><hr/></div>
-					<div className="space" id="a4">A</div>
+					<div className="space" id="a4"></div>
 					<div className="line" id="g4"><hr/></div>
-					<div className="space" id="f4">F</div>
+					<div className="space" id="f4"></div>
 					<div className="line" id="e4"><hr/></div>
 					<div className="space" id="d4">D</div>
 					<div className="ledger-line" id="c4">C</div>
 				</div>
+			</div>
 			);
 	}
 }
