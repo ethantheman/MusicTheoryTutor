a84b5338008767340df900d5c499055e0c017b26
Ethan Lipkind <ethan.lipkind@gmail.com>
2018-01-11 10:00:51 -0500

    update todo list, add clef pngs to the staff (need to fix resizing window issues)

--- client/src/grandStaff.jsx
+++ client/src/grandStaff.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Note from './Note.jsx';
+import $ from 'jquery';
 
 class GrandStaff extends React.Component {
 	constructor(props) {
@@ -10,29 +11,35 @@ class GrandStaff extends React.Component {
 	render() {
 		return (
 			<div>
-				<div className="space" id="g5"></div>
-				<div className="line" id="f5"></div>
-				<div className="space" id="e5"></div>
-				<div className="line" id="d5"></div>
-				<div className="space" id="c5"></div>
-				<div className="line" id="b4"></div>
-				<div className="space" id="a4"></div>
-				<div className="line" id="g4"></div>
-				<div className="space" id="f4"></div>
-				<div className="line" id="e4"></div>
-				<div className="space" id="d4"></div>
-				<div className="ledger-line" id="c4"><Note changeNote={this.props.changeNote}/></div>
-				<div className="space" id="b3"></div>
-				<div className="line" id="a3"></div>
-				<div className="space" id="g3"></div>
-				<div className="line" id="f3"></div>
-				<div className="space" id="e3"></div>
-				<div className="line" id="d3"></div>
-				<div className="space" id="c3"></div>
-				<div className="line" id="b2"></div>
-				<div className="space" id="a2"></div>
-				<div className="line" id="g2"></div>
-				<div className="space" id="f2"></div>
+				<div>
+					<img src="images/treble.png" className="trebleClef"></img>
+					<img src="images/Bass.png" className="bassClef"></img>
+				</div>
+				<div>
+					<div className="space" id="g5"></div>
+					<div className="line" id="f5"></div>
+					<div className="space" id="e5"></div>
+					<div className="line" id="d5"></div>
+					<div className="space" id="c5"></div>
+					<div className="line" id="b4"></div>
+					<div className="space" id="a4"></div>
+					<div className="line" id="g4"></div>
+					<div className="space" id="f4"></div>
+					<div className="line" id="e4"></div>
+					<div className="space" id="d4"></div>
+					<div className="ledger-line" id="c4"><Note changeNote={this.props.changeNote}/></div>
+					<div className="space" id="b3"></div>
+					<div className="line" id="a3"></div>
+					<div className="space" id="g3"></div>
+					<div className="line" id="f3"></div>
+					<div className="space" id="e3"></div>
+					<div className="line" id="d3"></div>
+					<div className="space" id="c3"></div>
+					<div className="line" id="b2"></div>
+					<div className="space" id="a2"></div>
+					<div className="line" id="g2"></div>
+					<div className="space" id="f2"></div>
+				</div>
 			</div>
 			);
 	}
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -9,23 +9,30 @@ class App extends React.Component {
 		super(props);
 		this.state = {
 			notes: ["C4"], // addNote button will append a new note to this array. will need some way to distinguish notes from each other...maybe change this to an object?
-			// selectedNote should probably be refactored to keep track of the index of the selected note in the state.notes array, rather than the name of the note.
 			selectedNote: "C4" // may need to refactor note selection to keep track of this in parent component...
 		}
 		this.changeNote=this.changeNote.bind(this);
 		this.getCursorPosition=this.getCursorPosition.bind(this);
+		this.addNote=this.addNote.bind(this);
 	}
 
 	getCursorPosition(e) {
+		// use the coordinates clicked to lookup which div was clicked
 		console.log('click! x: ', e.pageX, 'y: ', e.pageY);
 	}
 
-	changeNote(newNote) {
+	addNote(newNote) {
+		// eventually, get newNote using getCursorPosition
+		let oldNotes = this.state.notes;
+		oldNotes.push("C4");
+		this.setState({
+			notes: oldNotes
+		});
+	}
+
+	changeNote(newNote, index) {
 		// this function will be passed via props to the Note component
 		// onSelect, the Note will execute the function and update the parent component state
-
-		// will need some way of distinguishing between the notes in this.state.notes...
-		// but just get this working for one note for now.
 
 		this.setState({
 			notes: [newNote]
@@ -36,10 +43,10 @@ class App extends React.Component {
 		return (
 		<div onClick={this.getCursorPosition}>
 			<div>
-					<h1>Interval Buddy</h1>
+				<h1>Interval Buddy</h1>
 			</div>
 			<br/>
-			<GrandStaff changeNote={this.changeNote}/>
+			<GrandStaff changeNote={this.changeNote} notes={this.state.notes}/>
 			<div className="noteNameContainer">
 				<div className="noteName">{this.state.notes[0]}</div>
 			</div>
